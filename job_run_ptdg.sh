36d1ea2dde849b33f311793aa3717c36b0576968
Valentina Cristoferi <vale.cristoferi@libero.it>
2026-04-23 17:29:05 +0200

    fix job to run anomaly detection+plot

--- job_run_ptdg.sh
+++ job_run_ptdg.sh
@@ -1,49 +1,89 @@
 #!/bin/bash
 ### LSF options
-#BSUB -q gpuv100
-#BSUB -J tgn_mem_th
-#BSUB -o tgn_mem_theia_%J.out
-#BSUB -e tgn_mem_theia_%J.err
-#BSUB -n 4
+#BSUB -q gpua100 
+#BSUB -J TGN_5_1600
+#BSUB -o tgn_base_%J.out
+#BSUB -e tgn_base_%J.err
+#BSUB -n 8
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=32GB]"
-#BSUB -M 32GB
-#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "rusage[mem=12GB]"
+#BSUB -M 12GB
+#BSUB -gpu "num=1:mode=exclusive_process:mps=yes"
 #BSUB -W 24:00
+#BSUB -B
+#BSUB -N
 
 set -euo pipefail
 
 module purge
 
+module load cuda/11.7
+
 # Conda setup
 source ~/miniforge3/bin/activate
 conda activate ctdg
-export LD_LIBRARY_PATH="$CONDA_PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-export LD_PRELOAD="$CONDA_PREFIX/lib/libstdc++.so.6${LD_PRELOAD:+:$LD_PRELOAD}"
-export NUM_CPUS=4
-export NUM_GPUS=1
 
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export NUM_GPUS=1
+export NUM_CPUS=8
+export NUM_GPUS_PER_TASK=0.25
+export NUM_CPUS_PER_TASK=2
+
+SPLIT="05"
 SAVE_DIR=$(pwd)
+
+SAVE_DIR_BASE="$(pwd)/anomaly_detect"
+PRED_DIR="$(pwd)/TGN/ckpt"
+GROUND_TRUTH="/work3/s253892/ProvIDS/darpa_labelling/groundtruth"
 
 # Repo root
 cd "/work3/s253892/ProvIDS"
-echo "Repo CWD: $(pwd)"
-
-python -c "import torch, pandas, numpy, scipy, sklearn; import torch_geometric; print('imports ok')"
 
 # Run training (from README)
 cd src
 python -u main.py \
-  --data_name "darpa_theia_0to25" \
+  --data_name "darpa_theia_$SPLIT" \
   --model "TGN" \
-  --parallelism 2 \
-  --batch 200 \
-  --epochs 50 \
+  --batch 1600 \
+  --epochs 100 \
   --patience 5 \
+  --num_runs 1 \
   --save_dir ${SAVE_DIR} \
   --data_dir "/work3/s253892/ProvIDS/DATA/DATA" \
-  --num_runs 1 \
   --metric auc \
-  --memory_enhancement 1
+  --verbose
+
+
+N_TOT=$(find "$PRED_DIR" -maxdepth 1 -type f -name "split_conf_*" | wc -l)
+N_SEED=$(find "$PRED_DIR" -maxdepth 1 -type f -name "split_conf_0_*" | wc -l)
+N_CONF=$(( N_TOT / N_SEED ))
+
+
+for (( i=0; i < $N_CONF; i++ ))
+do
+    SAVE_DIR_ANOM="$SAVE_DIR_BASE/conf_${i}"
+
+    mkdir -p "$SAVE_DIR_ANOM"
+
+    python -u anomaly_detection.py \
+        --prediction_folder "$PRED_DIR" \
+        --ground_truth_path "$GROUND_TRUTH" \
+        --save_folder "$SAVE_DIR_ANOM" \
+        --model_name TGN \
+        --dataset THEIA \
+        --conf_id "$i" \
+        --split "$SPLIT" \
+        --num_seeds "$N_SEED"
+done
+
+cd "../tools"
+
+python plot_training_curves.py \
+    --ckpt "$PRED_DIR" \
+    --out "$SAVE_DIR/training_curves.png" \
+    --title "TGN ($SPLIT% training set)"
 
 echo "Job finished at $(date)"
